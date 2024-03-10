9bbd5427e47bd0805f688e5e984527ca80c7f973
Emmanuel Buraimoh <nuelity@gmail.com>
2024-03-10 15:19:25 +0100

    Update enrichment on python SDK

--- unit/models/authorization.py
+++ unit/models/authorization.py
@@ -35,7 +35,8 @@ class AuthorizationDTO(object):
                                 attributes.get("paymentMethod"), attributes.get("digitalWallet"),
                                 attributes.get("cardVerificationData"), attributes.get("cardNetwork"),
                                 attributes.get("tags"), relationships, attributes["merchant"].get("id"),
-                                attributes.get("declineReason"), attributes.get("cashWithdrawalAmount")), RichMerchantData.from_json_api(attributes.get("richMerchantData"))
+                                attributes.get("declineReason"), attributes.get("cashWithdrawalAmount"),
+                                RichMerchantData.from_json_api(attributes.get("richMerchantData")))
 
 
 class ListAuthorizationParams(UnitParams):
